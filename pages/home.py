85656a5bc0d5686af1db2bdc1a1ba1def02bc91a
svaksn22 <135360340+svaksn22@users.noreply.github.com>
2025-05-11 20:27:14 +0000

    修正

--- pages/home.py
+++ pages/home.py
@@ -66,9 +66,7 @@ def main():
         st.write(f"登校：{st.session_state.start_time}")
         st.write(f"帰宅：{st.session_state.end_time}")
         st.write("今日の研究内容")
-        with open("log/state.txt", "r") as f:
-            line = f.readline()
-            st.write(line.split(" ")[3].strip())
+        st.write(st.session_state.report)
     
 
 if __name__ == "__main__":
